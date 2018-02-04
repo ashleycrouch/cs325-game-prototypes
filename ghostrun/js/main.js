912f18619a8f11a66db5e5260a52a139ecb9507e
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-02-04 18:03:50 -0500

    ???

--- ghostrun/js/main.js
+++ ghostrun/js/main.js
@@ -81,12 +81,12 @@ window.onload = function() {
         bouncy = game.add.sprite(game.world.centerX, game.world.centerY, 'ghost');
 
         game.create.texture('wall', wall, pixelWidth, pixelHeight);
-        bouncy = game.add.sprite(game.world.centerX, 0, 'wall');
+        game.add.sprite(game.world.centerX, 0, 'wall');
 
 
 
         // Create a sprite at the center of the screen using the 'logo' image.
-        //bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'ghost' );
+        //bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
