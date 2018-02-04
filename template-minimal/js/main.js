0fc23b1eb0acbafb5c112fe1f92a22a866c6cf39
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-02-04 15:51:04 -0500

    trying to change picture

--- template-minimal/js/main.js
+++ template-minimal/js/main.js
@@ -14,7 +14,6 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image('ghost', 'assets/tempghost.jpg');
         game.load.image( 'logo', 'assets/phaser.png' );
     }
     
@@ -22,7 +21,7 @@ window.onload = function() {
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'ghost' );
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
