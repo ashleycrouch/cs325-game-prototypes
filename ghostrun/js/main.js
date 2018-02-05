b88f131a1c24a78b0b387f2a86165fee203460b2
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-02-05 14:59:23 -0500

    what

--- ghostrun/js/main.js
+++ ghostrun/js/main.js
@@ -12,88 +12,127 @@ window.onload = function() {
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     function preload() {
-        
-        //load the images in here
-        //game.load.image('sky', 'assets/sky.png');
-        //game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        // Load an image and call it 'logo'.
+        game.load.image( 'logo', 'assets/phaser.png' );
     }
+    
+    var bouncy;
+    
     function create() {
-
-        var pixelWidth = 6;
-        var pixelHeight = 6;
-
-        var ghost = [
-            '...111111...',
-            '..12222221..',
-            '.1222222221.',
-            '122222222221',
-            '122222222221',
-            '122222222221',
-            '122222222221',
-            '122222222221',
-            '122222222221',
-            '122222222221',
-            '122222222221',
-            '122222222221',
-            '112121212121',
-            '1.1.1.1.1.1.'
-        ];
-
-        // var ground = [
-        //     '44444444444444444444444444443',
-        //     '44444444444444444444444444443',
-        //     '44444444444444444444444444443'
-        // ]
-
-
-        game.create.texture('ghost', ghost, pixelWidth, pixelHeight);
-        //game.create.texture('ground', ground, pixelWidth, pixelHeight);
-        game.add.sprite(game.world.centerX, game.world.centerY, 'ghost');
-
-        //uses the Arcade physics system
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-
-        //add a background
-        //game.add.sprite(0, 0, 'sky');
-
-        //the platforms group contains the ground and the two ledges we can jump on
-        platforms = game.add.group();
-
-        //we will enable physics for any object that is created in this group
-        platforms.enableBody = true;
-        //enableBody = add rigidbody?
-
-        //here we create the ground
-        var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-        //scale it to fit the width of the game
-        ground.scale.setTo(2, 2);
-
-        //this stops it from falling away when you jump on it
-        ground.body.immovable = true;
-
-        //now let's create two ledges
-        var ledge = platforms.create(400, 400, 'ground');
-
-        ledge.body.immovable = true;
-        ledge = platforms.create(-150, 250, 'ground');
-        ledge.body.immovable = true;
-
-        //the player and its settings
-        player = game.add.sprite(32, game.world.height - 150, 'ghost');
-
-        //enable physics on the player
-        game.physics.arcade.enable(player);
+        // Create a sprite at the center of the screen using the 'logo' image.
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        // Anchor the sprite at its center, as opposed to its top-left corner.
+        // so it will be truly centered.
+        bouncy.anchor.setTo( 0.5, 0.5 );
         
-        //player physics has properties
-        player.body.bounce.y = 0.2;
-        player.body.gravity.y = 300;
-        player.body.collideWorldBounds = true;
-
-        //add animations
-        //player.animations.add('left', [0, 1, 2, 3], 10, true);
-        //player.animations.add('right', [5, 6, 7, 8], 10, true);
+        // Turn on the arcade physics engine for this sprite.
+        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
+        // Make it bounce off of the world bounds.
+        bouncy.body.collideWorldBounds = true;
+        
+        // Add some text using a CSS style.
+        // Center it in X, and position its top 15 pixels from the top of the world.
+        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        text.anchor.setTo( 0.5, 0.0 );
     }
+    
+    function update() {
+        // Accelerate the 'logo' sprite towards the cursor,
+        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
+        // in X or Y.
+        // This function returns the rotation angle that makes it visually match its
+        // new trajectory.
+        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+    }
+    
+    
+    
+    
+    
+    // function preload() {
+    //     game.load.image('test', 'assets/tempghost.jpg');
+    //     //load the images in here
+    //     //game.load.image('sky', 'assets/sky.png');
+    //     //game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    // }
+    // function create() {
+
+    //     var pixelWidth = 6;
+    //     var pixelHeight = 6;
+
+    //     var ghost = [
+    //         '...111111...',
+    //         '..12222221..',
+    //         '.1222222221.',
+    //         '122222222221',
+    //         '122222222221',
+    //         '122222222221',
+    //         '122222222221',
+    //         '122222222221',
+    //         '122222222221',
+    //         '122222222221',
+    //         '122222222221',
+    //         '122222222221',
+    //         '112121212121',
+    //         '1.1.1.1.1.1.'
+    //     ];
+
+    //     // var ground = [
+    //     //     '44444444444444444444444444443',
+    //     //     '44444444444444444444444444443',
+    //     //     '44444444444444444444444444443'
+    //     // ]
+
+
+    //     game.create.texture('test', ghost, pixelWidth, pixelHeight);
+    //     //game.create.texture('ground', ground, pixelWidth, pixelHeight);
+    //     game.add.sprite(game.world.centerX, game.world.centerY, 'test');
+
+    //     //uses the Arcade physics system
+    //     game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //     //add a background
+    //     //game.add.sprite(0, 0, 'sky');
+
+    //     //the platforms group contains the ground and the two ledges we can jump on
+    //     platforms = game.add.group();
+
+    //     //we will enable physics for any object that is created in this group
+    //     platforms.enableBody = true;
+    //     //enableBody = add rigidbody?
+
+    //     //here we create the ground
+    //     var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+    //     //scale it to fit the width of the game
+    //     ground.scale.setTo(2, 2);
+
+    //     //this stops it from falling away when you jump on it
+    //     ground.body.immovable = true;
+
+    //     //now let's create two ledges
+    //     var ledge = platforms.create(400, 400, 'ground');
+
+    //     ledge.body.immovable = true;
+    //     ledge = platforms.create(-150, 250, 'ground');
+    //     ledge.body.immovable = true;
+
+    //     //the player and its settings
+    //     // player = game.add.sprite(32, game.world.height - 150, 'ghost');
+
+    //     // //enable physics on the player
+    //     // game.physics.arcade.enable(player);
+        
+    //     // //player physics has properties
+    //     // player.body.bounce.y = 0.2;
+    //     // player.body.gravity.y = 300;
+    //     // player.body.collideWorldBounds = true;
+
+    //     //add animations
+    //     //player.animations.add('left', [0, 1, 2, 3], 10, true);
+    //     //player.animations.add('right', [5, 6, 7, 8], 10, true);
+    // }
 
       
     
@@ -166,7 +205,7 @@ window.onload = function() {
     
     function update() {
         //collide the player and the stars with the platforms
-        var hitPlatform = game.physics.arcade.collide(player, platforms);
+        //var hitPlatform = game.physics.arcade.collide(player, platforms);
 
 
 
