9817772ad8a7768599b5ce6d1dd3da57eaad5a02
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-04-04 15:45:37 -0400

    adding stuff

--- weirdcheckers/js/Game.js
+++ weirdcheckers/js/Game.js
@@ -27,11 +27,62 @@ GameStates.makeGame = function( game, shared ) {
     return {
 
         create: function () {
+            this.fieldArray = [];
+            this.fieldGroup = this.add.group();
+            this.score = 0;
+            this.bestScore = localStorage.getItem(gameOptions.localStorageName == null ? 0 : localStorage.getItem(gameOptions.localStorageName));
+            for(var i = 0; i < 4; i++)
+            {
+                this.fieldArray[i] = [];
+                for(var j = 0; j < 4; j++)
+                {
+                    var spot = this.add.sprite(this.tileDestination(j, COL), this.tileDestination(i, ROW), "spot")
+                    var tile = this.add.sprite(this.tileDestination(j, COL), this.tileDestination(i, ROW), "tiles");
+                    tile.alpha = 0;
+                    tile.visible = 0;
+                    this.fieldGroup.add(tile);
+                    this.fieldArray[i][j] = {
+                        tileValue: 0,
+                        tileSprite: tile,
+                        canUpgrade: true
+                    }
+                }
+            }
+
+
 
             backgroundSprite = game.add.tileSprite(0,0,game.width,game.height,'bg');
 
             //game = new Phaser.Game(480,800,Phaser.AUTO,'blocks-game',{preload:preload, create:create});
     
+        },
+
+        addChecker: function(){
+            var emptyTiles = [];
+            for(var i = 0; i < 4; i++){
+                for(var j = 0; j < 4; j++){
+                    if(this.fieldArray[i][j].tileValue == 0){
+                        emptyTiles.push({
+                            row: i,
+                            col: j
+                        })
+                    }
+                }
+            }
+            if(emptyTiles.length > 0){
+                var chosenTile = Phaser.Utils.Array.GetRandomElement(emptyTiles);
+                this.fieldArray[chosenTile.row][chosenTile.col].tileValue = 1;
+                this.fieldArray[chosenTile.row][chosenTile.col].tileSprite.visible = true;
+                this.fieldArray[chosenTile.row][chosenTile.col].tileSprite.setFrame(0);
+                this.tweens.add({
+                    targets: [this.fieldArray[chosenTile.row][chosenTile.col].tileSprite],
+                    alpha: 1,
+                    duration: gameOptions.tweenSpeed,
+                    onComplete: function(tween){
+                        tween.parent.scene.canMove = true;
+                    },
+                });
+            }
         },
     
         update: function () {
--- weirdcheckers/js/main.js
+++ weirdcheckers/js/main.js
@@ -1,20 +1,78 @@
 "use strict";
+var game;
+var gameOptions = {
+    tileSize: 200,
+    tweenSpeed: 50,
+    localStorageName: "highscore"
+}
 
+var ROW = 0;
+var COL = 1;
 window.onload = function() {
 
 	//	Create your Phaser game and inject it into the 'game' div.
 	//	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
 	var game = new Phaser.Game(480,800,Phaser.AUTO,'blocks-game',{preload:preload, create:create});
     
-	var ROW = 0;
-	var COL = 1;
 	var gameConfig = {
         type: Phaser.CANVAS,
         width: gameOptions.tileSize * 4,
-        height: (gameOptions.tileSize * 4),
-        backgroundColor: 0xecf0f1
+        height: gameOptions.tileSize * 4,
+		backgroundColor: 0xecf0f1
+		scene: [preloadAssets, playGame]
+	};
+	game = new Phaser.Game(gameConfig);
+    window.focus()
+    resize();
+    window.addEventListener("resize", resize, false);
+}
+
+var preloadAssets = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+    function preloadAssets(){
+        Phaser.Scene.call(this, {key: "PreloadAssets"});
+    },
+    preload: function(){
+        this.load.image("spot", "assets/sprites/spot.png");
+        this.load.image("gametitle", "assets/sprites/gametitle.png");
+        this.load.image("restart", "assets/sprites/restart.png");
+        this.load.image("scorepanel", "assets/sprites/scorepanel.png");
+        this.load.image("scorelabels", "assets/sprites/scorelabels.png");
+        this.load.image("logo", "assets/sprites/logo.png");
+        this.load.image("howtoplay", "assets/sprites/howtoplay.png");
+        this.load.spritesheet("tiles", "assets/sprites/tiles.png", {
+            frameWidth: gameOptions.tileSize,
+            frameHeight: gameOptions.tileSize
+        });
+        this.load.bitmapFont("font", "assets/fonts/font.png", "assets/fonts/font.fnt");
+        this.load.audio("move", ["assets/sounds/move.ogg", "assets/sounds/move.mp3"]);
+        this.load.audio("grow", ["assets/sounds/grow.ogg", "assets/sounds/grow.mp3"]);
+    },
+    create: function(){
+        this.scene.start("PlayGame");
     }
-	var shared = {};
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var shared = {};
 	
 	game.state.add( 'Boot', GameStates.makeBoot( game ) );
 	game.state.add( 'Preloader', GameStates.makePreloader( game ) );
@@ -24,11 +82,5 @@ window.onload = function() {
 	//	Now start the Boot state.
 	game.state.start('Boot');
 
-}
-
-var gameOptions = {
-    tileSize: 200,
-    tweenSpeed: 50,
-    localStorageName: "highscore"
 }
 
