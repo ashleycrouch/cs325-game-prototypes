ad6d1272f11b20e89297a888fafe37608b606cde
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-05-03 12:02:57 -0400

    changed stuff

--- suityourselfiterate/js/Game.js
+++ suityourselfiterate/js/Game.js
@@ -8,12 +8,18 @@ var spadeButton;
 var clubButton;
 
 var enemyCards;
+var enemyClub;
+var enemyDiamond;
+var enemyHeart;
+var enemySpade;
 
 var playerHealthText;
 var enemyHealthText;
 
 var playerDiamondText;
 var enemyDiamondText;
+
+var playerTurnText;
 
 var baseDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
@@ -35,10 +41,10 @@ GameStates.makeGame = function(game, shared)
 
     function Player()
     {
-        let hearts = shuffle(baseDeck);
-        let diamonds = shuffle(baseDeck);
-        let clubs = shuffle(baseDeck);
-        let spades = shuffle(baseDeck);
+        let hearts = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
+        let diamonds = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
+        let clubs = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
+        let spades = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
 
         let heartsUsed = [];
         let diamondsUsed = [];
@@ -47,6 +53,42 @@ GameStates.makeGame = function(game, shared)
 
         let heartStash = 20;
         let diamondStash = 0;
+
+            //basic functions for gaining/losing hearts/diamonds
+            this.gainHearts = function(val)
+            {
+                heartStash += val;
+            }
+        
+            this.loseHearts = function(val)
+            {
+                heartStash -= val;
+                if(heartStash < 0)
+                {
+                    heartStash = 0;
+                    playerTurnText.text = 'Game over';
+                    //player loses the game
+                }
+            }
+        
+            this.gainDiamonds = function(val)
+            {
+                diamondStash += val;
+                if(diamondStash >= 20)
+                {
+                    playerTurnText.text = 'Game over';
+                    //player wins the game
+                }
+            }
+        
+            this.loseDiamonds = function(val)
+            {
+                diamondStash -= val;
+                if(diamondStash < 0)
+                {
+                    diamondStash = 0;
+                }
+            }
 
         this.getHStash = function()
         {
@@ -61,14 +103,31 @@ GameStates.makeGame = function(game, shared)
             //handles the hearts deck
         this.useHearts = function()
         {
+            //console.log(sprite);
             let heartVal = hearts.pop();
-            var newText = game.add.text(heartButton.x, heartButton.y, 'You drew a ' + heartVal, { fill: '#ef0500ff' });
-                game.time.events.add(2000, function() {
+            if(allowInput)
+            {
+                //let heartVal = hearts.pop();
+                var newText = game.add.text(game.world.centerX, game.world.centerY, 'You drew a ' + heartVal, { fill: '#ef0500ff' });
+                game.time.events.add(1, function() {
                     game.add.tween(newText).to({y: 0}, 1500, Phaser.Easing.Linear.None, true);
                     game.add.tween(newText).to({alpha: 0}, 1500, Phaser.Easing.Linear.None, true);
                 }, this);
-
-            //console.log(heartVal);
+            newText.destroy();
+            }
+            else
+            {
+                //let heartVal = hearts.pop();
+                game.add.tween(enemyHeart.scale).to( { x: 1, y: 1 }, 1, Phaser.Easing.Linear.None, true);
+                var newText = game.add.text(enemyHeart.x, enemyHeart.y, 'They drew a ' + heartVal, { fill: '#ef0500ff' });
+                game.time.events.add(1, function() {
+                    game.add.tween(newText).to({y: 0}, 1500, Phaser.Easing.Linear.None, true);
+                    game.add.tween(newText).to({alpha: 0}, 1500, Phaser.Easing.Linear.None, true);
+                }, this);
+            newText.destroy();
+            }
+            
+            console.log(heartVal);
             heartsUsed.push(heartVal);
             if(heartVal == 'K')
             {
@@ -90,10 +149,11 @@ GameStates.makeGame = function(game, shared)
                 if(heartsUsed.indexOf('J') == -1 && heartsUsed.indexOf('K') == -1)
                 {
                     heartsUsed = [];
-                    hearts = shuffle(baseDeck);
+                    hearts = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
                 }
                 else
                 {
+                    playerTurnText.text = 'Game over';
                     //player wins the game
                 }
             }
@@ -123,6 +183,27 @@ GameStates.makeGame = function(game, shared)
         this.useDiamonds = function()
         {
             let diamondVal = diamonds.pop();
+            if(allowInput)
+            {
+                //let heartVal = hearts.pop();
+                var newText = game.add.text(game.world.centerX, game.world.centerY, 'You drew a ' + diamondVal, { fill: '#ef0500ff' });
+                game.time.events.add(1, function() {
+                    game.add.tween(newText).to({y: 0}, 1500, Phaser.Easing.Linear.None, true);
+                    game.add.tween(newText).to({alpha: 0}, 1500, Phaser.Easing.Linear.None, true);
+                }, this);
+            newText.destroy();
+            }
+            else
+            {
+                //let heartVal = hearts.pop();
+                var newText = game.add.text(enemyDiamond.x, enemyDiamond.y, 'They drew a ' + diamondVal, { fill: '#ef0500ff' });
+                game.time.events.add(1, function() {
+                    game.add.tween(newText).to({y: 0}, 1500, Phaser.Easing.Linear.None, true);
+                    game.add.tween(newText).to({alpha: 0}, 1500, Phaser.Easing.Linear.None, true);
+                }, this);
+            newText.destroy();
+            }
+
             diamondsUsed.push(diamondVal);
             if(diamondVal == 'K')
             {
@@ -144,10 +225,11 @@ GameStates.makeGame = function(game, shared)
                 if(diamondsUsed.indexOf('J') == -1 && diamondsUsed.indexOf('K') == -1)
                 {
                     diamondsUsed = [];
-                    diamonds = shuffle(baseDeck);
+                    diamonds = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
                 }
                 else
                 {
+                    playerTurnText.text = 'Game over';
                     //player wins the game
                 }
             }
@@ -170,6 +252,10 @@ GameStates.makeGame = function(game, shared)
         //enemy is a Player
         this.useSpades = function(enemy)
         {
+            if(enemy)
+            {
+                let enemyObj = enemy;
+            }
             let spadeVal = spades.pop();
             spadesUsed.push(spadeVal);
             if(spadeVal == 'K')
@@ -180,11 +266,11 @@ GameStates.makeGame = function(game, shared)
                 {
                     spadeVal = spades.pop();
                     spadesUsed.push(spadeVal);
-                    enemy.loseHearts(spadeVal*4);
+                    this.enemyObj.loseHearts(spadeVal*4);
                 }
                 else
                 {
-                    enemy.loseHearts(spadeVal*2);
+                    this.enemyObj.loseHearts(spadeVal*2);
                 }
             }
             else if(spadeVal == 'Q')
@@ -192,10 +278,11 @@ GameStates.makeGame = function(game, shared)
                 if(spadesUsed.indexOf('J') == -1 && spadesUsed.indexOf('K') == -1)
                 {
                     spadesUsed = [];
-                    spades = shuffle(baseDeck);
+                    spades = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
                 }
                 else
                 {
+                    playerTurnText.text = 'Game over';
                     //player wins the game
                 }
             }
@@ -204,12 +291,12 @@ GameStates.makeGame = function(game, shared)
                 spadeVal = spadesUsed[spadesUsed.length-1];
                 if(spadeVal != null)
                 {
-                    enemy.loseHearts(spadeVal);
+                    this.enemyObj.loseHearts(spadeVal);
                 }
             }
             else
             {
-                enemy.loseHearts(spadeVal);
+                this.enemyObj.loseHearts(spadeVal);
             }
             allowInput = !allowInput;
         }
@@ -218,6 +305,10 @@ GameStates.makeGame = function(game, shared)
         //enemy is a Player
         this.useClubs = function(enemy)
         {
+            if(enemy)
+            {
+                let enemyObj = enemy;
+            }
             let clubVal = clubs.pop();
             clubsUsed.push(clubVal);
             if(clubVal == 'K')
@@ -228,12 +319,12 @@ GameStates.makeGame = function(game, shared)
                 {
                     clubVal = clubs.pop();
                     clubsUsed.push(clubVal);
-                    enemy.loseDiamonds(clubVal*4);
+                    this.enemyObj.loseDiamonds(clubVal*4);
                     this.gainDiamonds(clubVal*4);
                 }
                 else
                 {
-                    enemy.loseDiamonds(clubVal*2);
+                    this.enemyObj.loseDiamonds(clubVal*2);
                     this.gainDiamonds(clubVal*2);
                 }
             }
@@ -242,10 +333,11 @@ GameStates.makeGame = function(game, shared)
                 if(clubsUsed.indexOf('J') == -1 && clubsUsed.indexOf('K') == -1)
                 {
                     clubsUsed = [];
-                    clubs = shuffle(baseDeck);
+                    clubs = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']);
                 }
                 else
                 {
+                    playerTurnText.text = 'Game over';
                     //player wins the game
                 }
             }
@@ -254,50 +346,16 @@ GameStates.makeGame = function(game, shared)
                 clubVal = clubsUsed[clubsUsed.length-1];
                 if(clubVal != null)
                 {
-                    enemy.loseDiamonds(clubVal);
+                    this.enemyObj.loseDiamonds(clubVal);
                     this.gainDiamonds(clubVal)
                 }
             }
             else
             {
-                enemy.loseDiamonds(clubVal);
+                this.enemyObj.loseDiamonds(clubVal);
                 this.gainDiamonds(clubVal);
             }
             allowInput = !allowInput;
-        }
-
-        //basic functions for gaining/losing hearts/diamonds
-        this.gainHearts = function(val)
-        {
-            heartStash += val;
-        }
-    
-        this.loseHearts = function(val)
-        {
-            heartStash -= val;
-            if(heartStash < 0)
-            {
-                heartStash = 0;
-                //player loses the game
-            }
-        }
-    
-        this.gainDiamonds = function(val)
-        {
-            diamondStash += val;
-            if(diamondStash >= 20)
-            {
-                //player wins the game
-            }
-        }
-    
-        this.loseDiamonds = function(val)
-        {
-            diamondStash -= val;
-            if(diamondStash < 0)
-            {
-                diamondStash = 0;
-            }
         }
 }
     //array shuffling method
@@ -327,7 +385,8 @@ GameStates.makeGame = function(game, shared)
             player = new Player();
             playerHealthText = game.add.text(50, 550, 'Health = ' + player.getHStash(), { fill: '#ef0500ff' });
             playerDiamondText = game.add.text(250, 550, 'Diamond = ' + player.getDStash(), { fill: '#7444acff' });
-
+            playerTurnText = game.add.text(game.world.centerX, game.world.centerY, '', { fill: '#7444acff' });
+            
             enemy = new Player();
             enemyHealthText = game.add.text(650, 200, 'Health = ' + enemy.getHStash(), { fill: '#ef0500ff' });
             enemyDiamondText = game.add.text(400, 200, 'Diamond = ' + enemy.getDStash(), { fill: '#7444acff' });
@@ -357,33 +416,31 @@ GameStates.makeGame = function(game, shared)
 
             spadeButton = playerCards.create(500, 700, 'spade');
             spadeButton.anchor.setTo(0.5, 0.5);
-            spadeButton.events.onInputDown.add(player.useSpades, player);
+            spadeButton.events.onInputDown.add(player.useSpades, {enemyObj: enemy}, player);
             spadeButton.input.useHandCursor = true;
 
             clubButton = playerCards.create(700, 700, 'club');
             clubButton.anchor.setTo(0.5, 0.5);
-            clubButton.events.onInputDown.add(player.useClubs, player);
+            clubButton.events.onInputDown.add(player.useClubs, {enemyObj: enemy}, player);
             clubButton.input.useHandCursor = true;
 
             enemyCards = game.add.group();
 
-            let enemyClub = enemyCards.create(100, 100, 'club');
+            enemyClub = enemyCards.create(100, 100, 'club');
             enemyClub.anchor.setTo(0.5, 0.5);
             enemyClub.angle = 180;
 
-            let enemySpade = enemyCards.create(300, 100, 'spade');
+            enemySpade = enemyCards.create(300, 100, 'spade');
             enemySpade.anchor.setTo(0.5, 0.5);
             enemySpade.angle = 180;
 
-            let enemyDiamond = enemyCards.create(500, 100, 'diamond');
+            enemyDiamond = enemyCards.create(500, 100, 'diamond');
             enemyDiamond.anchor.setTo(0.5, 0.5);
             enemyDiamond.angle = 180;
 
-            let enemyHeart = enemyCards.create(700, 100, 'heart');
+            enemyHeart = enemyCards.create(700, 100, 'heart');
             enemyHeart.anchor.setTo(0.5, 0.5);
             enemyHeart.angle = 180;
-            //game.input.mouse.capture = true;
-            //game.input.mousePointer.x/.y
         },
 
         update: function () {
@@ -411,7 +468,20 @@ GameStates.makeGame = function(game, shared)
                     enemy.useSpades(player);
                 }
             }
-    
+
+            if(allowInput)
+            {
+                playerTurnText.text = 'Your turn!';
+            }
+            else
+            {
+                playerTurnText.text = 'Their turn!';
+            }
+            
+            playerDiamondText.text = 'Diamond = ' + player.getDStash();
+            playerHealthText.text = 'Health = ' + player.getHStash();
+            enemyDiamondText.text = 'Diamond = ' + enemy.getDStash();
+            enemyHealthText.text = 'Health = ' + enemy.getHStash();
         }
     }
 };
